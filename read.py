1126b1fbf566f6ce98bda08d6249fcaff23b8370
Nathan Perry <avaglir@gmail.com>
2018-01-23 17:12:07 -0500

    switch formats

--- read.py
+++ read.py
@@ -13,8 +13,8 @@ conn = connector.connect(user=config['username'],
 
 with open('/dev/ttyUSB0') as f:
     cursor = conn.cursor()
-    light_insert = 'INSERT INTO light (measurement) VALUES (%f)'
-    temp_insert = 'INSERT INTO temperature (measurement) VALUES (%d)'
+    light_insert = 'INSERT INTO light (measurement) VALUES (%d)'
+    temp_insert = 'INSERT INTO temperature (measurement) VALUES (%f)'
 
     ct = 0
     acc = 0
